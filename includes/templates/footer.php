2129191ad30d9befd384ca572fbbb9ef4ebd3ff4
feruzu <fernanda23demarzo@gmail.com>
2023-06-14 01:54:43 -0300

    footer php

---
+++ includes/templates/footer.php
@@ -0,0 +1,14 @@
+<footer class="footer">
+      <div>
+        <a href="#">
+          <img src="./img/Logo_nike.svg" alt="Nike" width="70" height="40">
+        </a>
+      </div>
+
+      <div>
+        <img src="img/mapa.svg" alt="mapa">
+        <img src="img/contacto.svg" alt="contacto">
+        <img src="img/facebook.svg" alt="facebook">
+        <img src="img/instagram.svg" alt="instagram">
+      </div>
+    </footer>
