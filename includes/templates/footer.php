f12ce8f3fd27315d6307c6c36de64d567eb69283
feruzu <fernanda23demarzo@gmail.com>
2023-06-15 00:11:38 -0300

    Optimice código

--- includes/templates/footer.php
+++ includes/templates/footer.php
@@ -1,14 +1,22 @@
 <footer class="footer">
       <div>
         <a href="#">
-          <img src="./img/Logo_nike.svg" alt="Nike" width="70" height="40">
+          <img src="/././img/Logo_nike.svg" alt="Nike" width="70" height="40">
         </a>
       </div>
 
       <div>
-        <img src="img/mapa.svg" alt="mapa">
-        <img src="img/contacto.svg" alt="contacto">
-        <img src="img/facebook.svg" alt="facebook">
-        <img src="img/instagram.svg" alt="instagram">
+        <img src="/./img/mapa.svg" alt="mapa">
+        <img src="/./img/contacto.svg" alt="contacto">
+        <img src="/./img/facebook.svg" alt="facebook">
+        <img src="/./img/instagram.svg" alt="instagram">
       </div>
+
+
+      
+    <!--  Bootstrap   -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz"
+        crossorigin="anonymous"></script>
+    <script src="/js/app.js"></script>
     </footer>
--- includes/templates/header.php
+++ includes/templates/header.php
@@ -1,10 +1,27 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/css/main.css">
+
+    <!--Fuentes-->
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500;600;800;900&display=swap" rel="stylesheet">
+
+    <!--  Bootstrap   -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+
+    <title>Nike</title>
+</head>
+
+<body ontouchstart=""> 
+  
 <header class="header">
         <div class="contenedor">
             <nav class="navbar navbar-dark bg-dark fixed-top">
                 <div class="container-fluid">
                    
                         <a class="navbar-brand" href="#">
-                          <img src="./img/Logo_nike.svg" alt="Nike" width="70" height="40">
+                          <img src="/./img/Logo_nike.svg" alt="Nike" width="70" height="40">
                         </a>
                      
                   <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar" aria-label="Toggle navigation">
@@ -35,3 +52,6 @@
 
         </div>
     </header>
+
+
+</body>
