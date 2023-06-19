ac1d1698578bb6527d230e50ceb86e5710a74ff9
feruzu <fernanda23demarzo@gmail.com>
2023-06-19 20:01:38 -0300

    Commit

--- admin/index.php
+++ admin/index.php
@@ -3,10 +3,10 @@
     includeTemplate('header');
     ?>
 
-<main class="container seccion">
+<main class="container seccion actualizar">
     <h1>Administrador de Calzados</h1>
 
-    <a href="/admin/propiedades/crear.php">Añadir</a>
+    <a href="/admin/propiedades/crear.php" class="añadir">Añadir</a>
 </main>
 
 
