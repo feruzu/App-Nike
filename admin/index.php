822921a6f2c92a263379e3601f6bfd7a2aa34662
feruzu <fernanda23demarzo@gmail.com>
2023-06-21 21:08:43 -0300

    Add index.php

--- admin/index.php
+++ admin/index.php
@@ -1,4 +1,16 @@
 <?php
+
+    // Importar la conexion 
+    require '../includes/config/database.php';
+    $db = conectarDB();
+
+    // Escribir el query
+    $query = "SELECT * FROM calzados";
+
+    // Consultar BD
+    $resultado =  mysqli_query($db, $query);
+
+
     require '../includes/funciones.php';
     includeTemplate('header');
     ?>
@@ -6,10 +18,41 @@
 <main class="container seccion actualizar">
     <h1>Administrador de Calzados</h1>
 
-    <a href="/admin/propiedades/crear.php" class="añadir">Añadir</a>
+    <a href="/admin/calzados/crear.php" class="añadir">Añadir</a>
+
+
+    <table class="productos">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Titulo</th>
+                <th>Imagen</th>
+                <th>Precio</th>
+                <th>Talle</th>
+                <th>Calzados</th>
+            </tr>
+        </thead>
+
+        <tbody>
+            <?php while($calzados = mysqli_fetch_assoc($resultado)): ?>
+            <tr>
+                <td><?php echo $calzados['id']; ?></td>
+                <td><?php echo $calzados['titulo']; ?></td>
+                <td> <img src="/imagenes/<?php echo $calzados['imagen']; ?> "class="imagen-tabla"></td>
+                <td>$<?php echo $calzados['precio']; ?></td>
+                <td><?php echo $calzados['talle']; ?></td>
+                <td>
+                    <a class="boton-borrar" href="">Borrar</a>
+                    <a class="boton-actualizar" href="admin/calzados/actualizar.php?id=<?php echo $calzados['id']; ?>">Actualizar</a>
+                </td>
+            </tr>
+            <?php endwhile; ?>
+        </tbody>
+    </table>
 </main>
 
-
 <?php
+    mysqli_close($db);
+
    includeTemplate('footer');
     ?>
